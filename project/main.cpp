98f755c7e58b1600ce354f0a19a115bed81884e0
tony14256 <tony14256@gmail.com>
2015-12-22 16:07:40 +0800

    read_file_2

--- project/main.cpp
+++ project/main.cpp
@@ -1,33 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include<string>
 #include<fstream>
 using namespace std;
 
 #define SIZE 200
 char line[SIZE];
 int Count = 0;
+string layer;
+double point;
 int main(){
     fstream fin;
     fin.open("D:/HomeWork/CG/FinalProject/project/blender_half_resampled_black.svg",ios::in);
     while(fin.getline(line,sizeof(line),'\n')){
 
         for(int i = 0; i < sizeof(line); i++){
-                if(line[i] == 'M'){
+                if(line[i] == 'l' && line[i + 4] == 'r'){
+                    for(int j = i ; j < (i + 6); j++){
+                        layer = layer + line[j];
+                    }
+                      cout<< layer ;
+                      layer = "";
+                }
+                else if(line[i] == 'M'){
                         printf("M:");
-                    for(int j = i ; j < (i+17); j++){
-                        if(isdigit(line[j]) || line[j] ==' ' || line[j] ==',' || line[j] =='.')
-                        printf("%c", line[j]);
+                    for(int j = i; j <= (i+17); j++){
+                        if(isdigit(line[j]) || line[j] =='.'){
+                            layer = layer + line[j];
+                        }
+                        else if(line[j] == ' ' || line[j] == ','){
+                            if(layer != ""){
+                                point = atof(layer.c_str());
+                                printf("%.3f", point);
+                                layer = "";
+                                if(line[j] == ','){
+                                    printf(", ");
+                                }
+                            }
+                        }
                     }
                 }
                 else if(line[i] == 'C' && line[i-1] == ' '){
                         printf("  C:");
                     for(int j = i ; j < (i+50); j++){
-                        if(isdigit(line[j]) || line[j] ==' ' || line[j] == ',' || line[j] == '.')
-                        printf("%c", line[j]);
+                        if(isdigit(line[j]) || line[j] =='.'){
+                            layer = layer + line[j];
+                        }
+                        else if(line[j] == ' ' || line[j] == ',' || line[j] == '"'){
+                            if(layer != ""){
+                                point = atof(layer.c_str());
+                                printf("%.3f", point);
+                                layer = "";
+                                if(line[j] == ','){
+                                    printf(",");
+                                }
+                                else if(line[j] == ' '){
+                                    printf(" ");
+                                }
+                            }
+                        }
                     }
                 }
-
         }
         Count++;
         printf("    %d", Count);
@@ -36,8 +70,6 @@ int main(){
             line[i] = '\0';
          }
     }
-
-
     printf("%d", Count);
     system("pause");
     return 0;
